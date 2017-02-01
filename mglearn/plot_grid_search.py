7af7b83115d6ff08d2e90528d589a97ed34c4ac0
rickiepark <haesunrpark@gmail.com>
2017-02-01 17:49:00 +0900

    fix bug

--- mglearn/plot_grid_search.py
+++ mglearn/plot_grid_search.py
@@ -16,26 +16,26 @@ def plot_cross_val_selection():
                   'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
     grid_search = GridSearchCV(SVC(), param_grid, cv=5)
     grid_search.fit(X_trainval, y_trainval)
-    results = pd.DataFrame(grid_search.results_)[15:]
+    results = pd.DataFrame(grid_search.cv_results_)[15:]
 
-    best = np.argmax(results.test_mean_score.values)
+    best = np.argmax(results.mean_test_score.values)
     plt.figure(figsize=(10, 3))
     plt.xlim(-1, len(results))
     plt.ylim(0, 1.1)
     for i, (_, row) in enumerate(results.iterrows()):
-        scores = row[['test_split%d_score' % i for i in range(5)]]
+        scores = row[['split%d_test_score' % i for i in range(5)]]
         marker_cv, = plt.plot([i] * 5, scores, '^', c='gray', markersize=5, alpha=.5)
-        marker_mean, = plt.plot(i, row.test_mean_score, 'v', c='none', alpha=1, markersize=10)
+        marker_mean, = plt.plot(i, row.mean_test_score, 'v', c='none', alpha=1, markersize=10)
         if i == best:
-            marker_best, = plt.plot(i, row.test_mean_score, 'o', c='red', fillstyle="none",
+            marker_best, = plt.plot(i, row.mean_test_score, 'o', c='red', fillstyle="none",
                                     alpha=1, markersize=20, markeredgewidth=3)
 
     plt.xticks(range(len(results)), [str(x).strip("{}").replace("'", "")
-                                     for x in grid_search.results_['params']], rotation=90)
-    plt.ylabel("Validation accuracy")
-    plt.xlabel("Parameter settings")
+                                     for x in grid_search.cv_results_['params']], rotation=90)
+    plt.ylabel("정확도")
+    plt.xlabel("파라미터 세팅")
     plt.legend([marker_cv, marker_mean, marker_best],
-               ["cv accuracy", "mean accuracy", "best parameter setting"], loc=(1.05, .4))
+               ["크로스 밸리데이션 정확도", "평균 정확도", "최적 파라미터 세팅"], loc=(1.05, .4))
 
 
 def plot_grid_search_overview():
@@ -53,7 +53,7 @@ def plot_grid_search_overview():
             end = start
             patchB = None
         annotation = ax.annotate(text, end, start, xycoords='axes pixels',
-                                 textcoords='axes pixels', size=20,
+                                 textcoords='axes pixels', size=10,
                                  arrowprops=dict(arrowstyle="-|>", fc="w",
                                                  ec="k", patchB=patchB,
                                                  connectionstyle="arc3,rad=0.0"),
@@ -63,18 +63,18 @@ def plot_grid_search_overview():
         plt.draw()
         return annotation
 
-    step = 100
-    grr = 400
+    step = 200
+    grr = 600
 
-    final_evaluation = draw(axes, "final evaluation", (5 * step, grr - 3 * step))
-    retrained_model = draw(axes, "retrained model", (3 * step, grr - 3 * step), final_evaluation)
-    best_parameters = draw(axes, "best parameters", (.5 * step, grr - 3 * step), retrained_model)
-    cross_validation = draw(axes, "cross-validation", (.5 * step, grr - 2 * step), best_parameters)
-    parameters = draw(axes, "parameter grid", (0.0, grr - 0), cross_validation)
-    training_data = draw(axes, "training data", (2 * step, grr - step), cross_validation)
-    draw(axes, "training data", (2 * step, grr - step), retrained_model)
-    test_data = draw(axes, "test data", (5 * step, grr - step), final_evaluation)
-    draw(axes, "data set", (3.5 * step, grr - 0.0), training_data)
-    data_set = draw(axes, "data set", (3.5 * step, grr - 0.0), test_data)
+    final_evaluation = draw(axes, "최종 평가", (5 * step, grr - 3 * step))
+    retrained_model = draw(axes, "최종 모델 학습", (3 * step, grr - 3 * step), final_evaluation)
+    best_parameters = draw(axes, "최적 파라미터", (.5 * step, grr - 3 * step), retrained_model)
+    cross_validation = draw(axes, "크로스 밸리데이션", (.5 * step, grr - 2 * step), best_parameters)
+    parameters = draw(axes, "파라미터 그리드", (0.0, grr - 0), cross_validation)
+    training_data = draw(axes, "훈련 데이터", (2 * step, grr - step), cross_validation)
+    draw(axes, "훈련 데이터", (2 * step, grr - step), retrained_model)
+    test_data = draw(axes, "테스트 데이터", (5 * step, grr - step), final_evaluation)
+    draw(axes, "데이터 세트", (3.5 * step, grr - 0.0), training_data)
+    data_set = draw(axes, "데이터 세트", (3.5 * step, grr - 0.0), test_data)
     plt.ylim(0, 1)
     plt.xlim(0, 1.5)
