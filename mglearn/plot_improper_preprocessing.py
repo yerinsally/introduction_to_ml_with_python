77634c05496bbf004d803236856f14e37e1b1976
rickiepark <haesunrpark@gmail.com>
2017-02-25 10:08:24 +0900

    finish chapter 6

--- mglearn/plot_improper_preprocessing.py
+++ mglearn/plot_improper_preprocessing.py
@@ -10,7 +10,7 @@ def make_bracket(s, xy, textxy, width, ax):
 
 
 def plot_improper_processing():
-    fig, axes = plt.subplots(2, 1, figsize=(15, 10))
+    fig, axes = plt.subplots(2, 1, figsize=(15, 8))
 
     for axis in axes:
         bars = axis.barh([0, 0, 0], [11.9, 2.9, 4.9], left=[0, 12, 15],
@@ -18,27 +18,28 @@ def plot_improper_processing():
         bars[2].set_hatch(r"")
         axis.set_yticks(())
         axis.set_frame_on(False)
-        axis.set_ylim(-.1, 6)
+        axis.set_ylim(-.1, 4.5)
         axis.set_xlim(-0.1, 20.1)
         axis.set_xticks(())
         axis.tick_params(length=0, labeltop=True, labelbottom=False)
-        axis.text(6, -.3, "training folds",
+        axis.text(6, -.3, "훈련 폴드",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(13.5, -.3, "validation fold",
+        axis.text(13.5, -.3, "검증 폴드",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(17.5, -.3, "test set",
+        axis.text(17.5, -.3, "테스트 세트",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
 
     make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[0])
     make_bracket("SVC fit", (6, 3), (6, 4), 12, axes[0])
     make_bracket("SVC predict", (13.4, 3), (13.4, 4), 2.5, axes[0])
 
-    axes[0].set_title("Cross validation")
-    axes[1].set_title("Test set prediction")
+    axes[0].set_title("크로스 밸리데이션")
+    axes[1].set_title("테스트 세트 예측")
 
     make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[1])
     make_bracket("SVC fit", (7.5, 3), (7.5, 4), 15, axes[1])
     make_bracket("SVC predict", (17.5, 3), (17.5, 4), 4.8, axes[1])
+    fig.subplots_adjust(hspace=.3)
 
 
 def plot_proper_processing():
@@ -54,16 +55,16 @@ def plot_proper_processing():
         axis.set_xlim(-0.1, 20.1)
         axis.set_xticks(())
         axis.tick_params(length=0, labeltop=True, labelbottom=False)
-        axis.text(6, -.3, "training folds", fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(13.5, -.3, "validation fold", fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(17.5, -.3, "test set", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(6, -.3, "훈련 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(13.5, -.3, "검증 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(17.5, -.3, "테스트 세트", fontdict={'fontsize': 14}, horizontalalignment="center")
       
     make_bracket("scaler fit", (6, 1.3), (6, 2.), 12, axes[0])
     make_bracket("SVC fit", (6, 3), (6, 4), 12, axes[0])
     make_bracket("SVC predict", (13.4, 3), (13.4, 4), 2.5, axes[0])
 
-    axes[0].set_title("Cross validation")
-    axes[1].set_title("Test set prediction")
+    axes[0].set_title("크로스 밸리데이션")
+    axes[1].set_title("테스트 세트 예측")
 
     make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[1])
     make_bracket("SVC fit", (7.5, 3), (7.5, 4), 15, axes[1])
