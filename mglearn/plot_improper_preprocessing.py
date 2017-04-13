9c0ad42886d8ed8d2475550b535b55b8382cc980
rickiepark <haesunrpark@gmail.com>
2017-04-13 17:28:13 +0900

    update pictures in chapter6

--- mglearn/plot_improper_preprocessing.py
+++ mglearn/plot_improper_preprocessing.py
@@ -14,19 +14,19 @@ def plot_improper_processing():
 
     for axis in axes:
         bars = axis.barh([0, 0, 0], [11.9, 2.9, 4.9], left=[0, 12, 15],
-                         color=['white', 'grey', 'grey'], hatch="//")
+                         color=['white', 'grey', 'grey'], hatch="//", edgecolor='k')
         bars[2].set_hatch(r"")
         axis.set_yticks(())
         axis.set_frame_on(False)
-        axis.set_ylim(-.1, 4.5)
-        axis.set_xlim(-0.1, 20.1)
+        axis.set_ylim(-0.5, 4.5)
+        axis.set_xlim(0, 20.1)
         axis.set_xticks(())
         axis.tick_params(length=0, labeltop=True, labelbottom=False)
-        axis.text(6, -.3, "훈련 폴드",
+        axis.text(6, -.8, "훈련 폴드",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(13.5, -.3, "검증 폴드",
+        axis.text(13.5, -.8, "검증 폴드",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(17.5, -.3, "테스트 세트",
+        axis.text(17.5, -.8, "테스트 세트",
                   fontdict={'fontsize': 14}, horizontalalignment="center")
 
     make_bracket("scaler fit", (7.5, 1.3), (7.5, 2.), 15, axes[0])
@@ -47,17 +47,17 @@ def plot_proper_processing():
 
     for axis in axes:
         bars = axis.barh([0, 0, 0], [11.9, 2.9, 4.9],
-                         left=[0, 12, 15], color=['white', 'grey', 'grey'], hatch="//")
+                         left=[0, 12, 15], color=['white', 'grey', 'grey'], hatch="//", edgecolor='k')
         bars[2].set_hatch(r"")
         axis.set_yticks(())
         axis.set_frame_on(False)
-        axis.set_ylim(-.1, 4.5)
-        axis.set_xlim(-0.1, 20.1)
+        axis.set_ylim(-.5, 4.5)
+        axis.set_xlim(0, 20.1)
         axis.set_xticks(())
         axis.tick_params(length=0, labeltop=True, labelbottom=False)
-        axis.text(6, -.3, "훈련 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(13.5, -.3, "검증 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
-        axis.text(17.5, -.3, "테스트 세트", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(6, -.8, "훈련 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(13.5, -.8, "검증 폴드", fontdict={'fontsize': 14}, horizontalalignment="center")
+        axis.text(17.5, -.8, "테스트 세트", fontdict={'fontsize': 14}, horizontalalignment="center")
       
     make_bracket("scaler fit", (6, 1.3), (6, 2.), 12, axes[0])
     make_bracket("SVC fit", (6, 3), (6, 4), 12, axes[0])
